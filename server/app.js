376a1674f85b20cc22f2972c0225f0fe82d7270a
Nikhil-Bhatt <nikhilbhatt220705@gmail.com>
2024-11-05 09:11:37 +0530

    Mongoose connection added

--- server/app.js
+++ server/app.js
@@ -1,20 +1,37 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./db";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import authRouter from './routes/auth-router';
+import authRouter from "./routes/auth-router";
 
 const app = express();
 const port = process.env.PORT;
 
+// Connect to MongoDB
+connectDB();
+
 // CORS middleware
-app.use(cors({origin: `http://localhost:${process.env.CLIENT_PORT || 5173}`}));
+app.use(
+  cors({
+    origin: `http://localhost:${process.env.CLIENT_PORT || 5173}`,
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+// Other middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // Auth routes
-app.use('/auth', authRouter);
+app.use("/auth", authRouter);
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });

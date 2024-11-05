78faf8d0d3e85f89f1ebd1f31c6ccd1cf722198a
Nikhil-Bhatt <nikhilbhatt220705@gmail.com>
2024-11-05 08:54:47 +0530

    Added auth & authorizzz

--- server/app.js
+++ server/app.js
@@ -1,10 +1,18 @@
-const express = require('express');
-const authRouter = require('./routes/auth-router');
-require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import authRouter from './routes/auth-router';
 
 const app = express();
 const port = process.env.PORT;
 
+// CORS middleware
+app.use(cors({origin: `http://localhost:${process.env.CLIENT_PORT || 5173}`}));
+
+// Auth routes
 app.use('/auth', authRouter);
 
 app.listen(port, () => {
--- server/routes/auth-router.js
+++ server/routes/auth-router.js
@@ -1,11 +1,15 @@
-const router = require('express').Router();
+import express from "express";
+import { register, login } from "../controllers/auth-controller";
+import authMiddleware from "../middlewares/auth-middleware";
+const router = express.Router();
 
-router.get('/login', (req, res) => {
-  res.send('Login Page');
-});
+// Public Routes
+router.post("/register", register);
+router.post("/login", login);
 
-router.get('/register', (req, res) => {
-  res.send('Register Page');
+// Protected Route Example
+router.get("/protected", authMiddleware, (req, res) => {
+  res.json({ message: `Welcome ${req.user.username}!` });
 });
 
 module.exports = router;
